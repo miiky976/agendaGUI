520ee73edb54d622b96bb483ae448f168e4eb462
miiky976 <mruiz3445@gmail.com>
2022-09-08 03:23:39 -0500

    Estilizado

--- window.py
+++ window.py
@@ -16,7 +16,6 @@ class CreateWindow:
         self.set_select()
 
     def update_entry(self):
-        print(self.pos)
         self.db.update(self.pos, nombrevar.get(), productovar.get(), numerovar.get(), preciovar.get())
         self.clear_entry()
 
@@ -40,62 +39,80 @@ class CreateWindow:
 
     def make_window(self):
         global nombrevar, productovar, numerovar, preciovar, select
-        win = Tk()
-        win.geometry("400x300")
+        root = Tk()
+        root.geometry("600x400")
+        root.config(bg="#1a232e")
 
-        frame1 = Frame(win)
+        fontsize=14
+
+        frame1 = Frame(root, bg="#1a232e")
         frame1.pack()
 
         # Text box for First Name
-        Label(frame1, text="Nombre").grid(row=0, column=0, sticky=W)
+        Label(frame1, text="Nombre", bg="#1a232e", fg="white", font=("Arial", fontsize)).grid(row=0, column=0, sticky=W)
         nombrevar = StringVar()
-        nombre = Entry(frame1, textvariable=nombrevar)
+        nombre = Entry(frame1, textvariable=nombrevar, bg="#516374", fg="white", font=("Arial", fontsize), borderwidth=0)
         nombre.grid(row=0, column=1, sticky=W)
 
         # Text box for Last Name
-        Label(frame1, text="Producto").grid(row=1, column=0, sticky=W)
+        Label(frame1, text="Producto", bg="#1a232e", fg="white", font=("Arial", fontsize)).grid(row=1, column=0, sticky=W)
         productovar = StringVar()
-        producto = Entry(frame1, textvariable=productovar)
+        producto = Entry(frame1, textvariable=productovar, bg="#516374", fg="white", font=("Arial", fontsize), borderwidth=0)
         producto.grid(row=1, column=1, sticky=W)
 
         # Text box for numero number
-        Label(frame1, text="Numero").grid(row=2, column=0, sticky=W)
+        Label(frame1, text="Numero", bg="#1a232e", fg="white", font=("Arial", fontsize)).grid(row=2, column=0, sticky=W)
         numerovar = StringVar()
-        numero = Entry(frame1, textvariable=numerovar)
+        numero = Entry(frame1, textvariable=numerovar, bg="#516374", fg="white", font=("Arial", fontsize), borderwidth=0)
         numero.grid(row=2, column=1, sticky=W)
 
         # Text box for numero number
-        Label(frame1, text="Precio").grid(row=3, column=0, sticky=W)
+        Label(frame1, text="Precio", bg="#1a232e", fg="white", font=("Arial", fontsize)).grid(row=3, column=0, sticky=W)
         preciovar = StringVar()
-        precio = Entry(frame1, textvariable=preciovar)
+        precio = Entry(frame1, textvariable=preciovar, bg="#516374", fg="white", font=("Arial", fontsize), borderwidth=0)
         precio.grid(row=3, column=1, sticky=W)
 
         # Section for action buttons
-        frame2 = Frame(win)
+        frame2 = Frame(root, bg="#1a232e")
         frame2.pack()
-        b1 = Button(frame2, text="Agregar", command=self.add_entry)
-        b2 = Button(frame2, text="Actualizar", command=self.update_entry)
-        b3 = Button(frame2, text="Eliminar", command=self.delete_entry)
-        b4 = Button(frame2, text="Limpiar Textos ", command=self.clear_entry)
+        b1 = Button(frame2, text="Agregar", command=self.add_entry, 
+            bg="#125230", fg="white", borderwidth=0, font=("Arial", fontsize), activebackground="#0F4428"
+            )
+        b2 = Button(frame2, text="Actualizar", command=self.update_entry, 
+            bg="#2F6161", fg="white", borderwidth=0, font=("Arial", fontsize), activebackground="#0F4444"
+            )
+        b3 = Button(frame2, text="Eliminar", command=self.delete_entry, 
+            bg="#69090C", fg="white", borderwidth=0, font=("Arial", fontsize), activebackground="#440F11"
+            )
+        b4 = Button(frame2, text="Limpiar Textos ", command=self.clear_entry, 
+            bg="#0E286B", fg="white", borderwidth=0, font=("Arial", fontsize), activebackground="#0F1E44"
+            )
         b1.pack(side=LEFT)
         b2.pack(side=LEFT)
         b3.pack(side=LEFT)
         b4.pack(side=LEFT)
 
         # Section for list of contacts
-        frame3 = Frame(win)
+        frame3 = Frame(root, bg="#1a232e")
         frame3.pack()
-        scroll = Scrollbar(frame3, orient=VERTICAL)
-        select = Listbox(frame3, yscrollcommand=scroll.set, height=6)
+        scrolly = Scrollbar(frame3, orient=VERTICAL)
+        scrollx = Scrollbar(frame3, orient=HORIZONTAL)
+        select = Listbox(frame3, yscrollcommand=scrolly.set, xscrollcommand=scrollx.set,
+            height=15, width=70,
+            bg="#354755", fg="white", border=0,
+            font=("Arial", fontsize)
+            )
         select.bind('<<ListboxSelect>>', self.which_selected)
-        scroll.config(command=select.yview)
-        scroll.pack(side=RIGHT, fill=Y)
+        scrolly.config(command=select.yview)
+        scrollx.config(command=select.xview)
+        scrolly.pack(side=RIGHT, fill=Y)
+        scrollx.pack(side=BOTTOM, fill=X)
         select.pack(side=LEFT, fill=BOTH, expand=1)
 
-        win.update()
-        win.minsize(win.winfo_width(), win.winfo_height())
-        win.title("Agenda")
-        return win
+        root.update()
+        root.minsize(root.winfo_width(), root.winfo_height())
+        root.title("Agenda")
+        return root
 
     def set_select(self):
         records = self.db.get_all()
