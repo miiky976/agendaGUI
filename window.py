69a4cdf9dbf7c5cde2e23f80d05044a8bf0ffbd1
miiky976 <mruiz3445@gmail.com>
2022-09-09 00:18:08 -0500

    Colores mas amigables

--- window.py
+++ window.py
@@ -38,54 +38,58 @@ class CreateWindow:
         preciovar.set(records[4])
 
     def make_window(self):
+        fontsize=12
+        backg = "white"
+        foreg = "black"
+        listback = "#D6EAF8"
+        textback = "#AED6F1"
+
         global nombrevar, productovar, numerovar, preciovar, select
         root = Tk()
         root.geometry("600x400")
-        root.config(bg="#1a232e")
+        root.config(bg=backg)
 
-        fontsize=14
-
-        frame1 = Frame(root, bg="#1a232e")
+        frame1 = Frame(root, bg=backg)
         frame1.pack()
 
         # Text box for First Name
-        Label(frame1, text="Nombre", bg="#1a232e", fg="white", font=("Arial", fontsize)).grid(row=0, column=0, sticky=W)
+        Label(frame1, text="Nombre", bg=backg, fg=foreg, font=("Arial", fontsize)).grid(row=0, column=0, sticky=W)
         nombrevar = StringVar()
-        nombre = Entry(frame1, textvariable=nombrevar, bg="#516374", fg="white", font=("Arial", fontsize), borderwidth=0)
+        nombre = Entry(frame1, textvariable=nombrevar, bg=textback, fg=foreg, font=("Arial", fontsize), borderwidth=0)
         nombre.grid(row=0, column=1, sticky=W)
 
         # Text box for Last Name
-        Label(frame1, text="Producto", bg="#1a232e", fg="white", font=("Arial", fontsize)).grid(row=1, column=0, sticky=W)
+        Label(frame1, text="Producto", bg=backg, fg=foreg, font=("Arial", fontsize)).grid(row=1, column=0, sticky=W)
         productovar = StringVar()
-        producto = Entry(frame1, textvariable=productovar, bg="#516374", fg="white", font=("Arial", fontsize), borderwidth=0)
+        producto = Entry(frame1, textvariable=productovar, bg=textback, fg=foreg, font=("Arial", fontsize), borderwidth=0)
         producto.grid(row=1, column=1, sticky=W)
 
         # Text box for numero number
-        Label(frame1, text="Numero", bg="#1a232e", fg="white", font=("Arial", fontsize)).grid(row=2, column=0, sticky=W)
+        Label(frame1, text="Numero", bg=backg, fg=foreg, font=("Arial", fontsize)).grid(row=2, column=0, sticky=W)
         numerovar = StringVar()
-        numero = Entry(frame1, textvariable=numerovar, bg="#516374", fg="white", font=("Arial", fontsize), borderwidth=0)
+        numero = Entry(frame1, textvariable=numerovar, bg=textback, fg=foreg, font=("Arial", fontsize), borderwidth=0)
         numero.grid(row=2, column=1, sticky=W)
 
         # Text box for numero number
-        Label(frame1, text="Precio", bg="#1a232e", fg="white", font=("Arial", fontsize)).grid(row=3, column=0, sticky=W)
+        Label(frame1, text="Precio", bg=backg, fg=foreg, font=("Arial", fontsize)).grid(row=3, column=0, sticky=W)
         preciovar = StringVar()
-        precio = Entry(frame1, textvariable=preciovar, bg="#516374", fg="white", font=("Arial", fontsize), borderwidth=0)
+        precio = Entry(frame1, textvariable=preciovar, bg=textback, fg=foreg, font=("Arial", fontsize), borderwidth=0)
         precio.grid(row=3, column=1, sticky=W)
 
         # Section for action buttons
-        frame2 = Frame(root, bg="#1a232e")
+        frame2 = Frame(root, bg=backg)
         frame2.pack()
         b1 = Button(frame2, text="Agregar", command=self.add_entry, 
-            bg="#125230", fg="white", borderwidth=0, font=("Arial", fontsize), activebackground="#0F4428"
+            bg="#ABEBC6", fg=foreg, borderwidth=0, font=("Arial", fontsize), activebackground="#82E0AA"
             )
         b2 = Button(frame2, text="Actualizar", command=self.update_entry, 
-            bg="#2F6161", fg="white", borderwidth=0, font=("Arial", fontsize), activebackground="#0F4444"
+            bg="#D2B4DE", fg=foreg, borderwidth=0, font=("Arial", fontsize), activebackground="#BB8FCE"
             )
         b3 = Button(frame2, text="Eliminar", command=self.delete_entry, 
-            bg="#69090C", fg="white", borderwidth=0, font=("Arial", fontsize), activebackground="#440F11"
+            bg="#F5B7B1", fg=foreg, borderwidth=0, font=("Arial", fontsize), activebackground="#F1948A"
             )
         b4 = Button(frame2, text="Limpiar Textos ", command=self.clear_entry, 
-            bg="#0E286B", fg="white", borderwidth=0, font=("Arial", fontsize), activebackground="#0F1E44"
+            bg="#E5E7E9", fg=foreg, borderwidth=0, font=("Arial", fontsize), activebackground="#D7DBDD"
             )
         b1.pack(side=LEFT)
         b2.pack(side=LEFT)
@@ -93,18 +97,18 @@ class CreateWindow:
         b4.pack(side=LEFT)
 
         # Section for list of contacts
-        frame3 = Frame(root, bg="#1a232e")
+        frame3 = Frame(root, bg=backg)
         frame3.pack()
         scrolly = Scrollbar(frame3, orient=VERTICAL)
         scrollx = Scrollbar(frame3, orient=HORIZONTAL)
         select = Listbox(frame3, yscrollcommand=scrolly.set, xscrollcommand=scrollx.set,
             height=15, width=70,
-            bg="#354755", fg="white", border=0,
+            bg=listback, fg=foreg, border=0,
             font=("Arial", fontsize)
             )
         select.bind('<<ListboxSelect>>', self.which_selected)
-        scrolly.config(command=select.yview)
-        scrollx.config(command=select.xview)
+        scrolly.config(command=select.yview, bg=listback)
+        scrollx.config(command=select.xview, bg=listback)
         scrolly.pack(side=RIGHT, fill=Y)
         scrollx.pack(side=BOTTOM, fill=X)
         select.pack(side=LEFT, fill=BOTH, expand=1)
